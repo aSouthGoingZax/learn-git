a6321cf160056cd4fc48d624b2a8c3e2d6b38cc5
Blaine Perston <96595637+aSouthGoingZax@users.noreply.github.com>
2021-12-29 21:37:24 +0000

    add some info

--- yt-tutorial.c
+++ yt-tutorial.c
@@ -39,7 +39,7 @@ void sayHi(char name[], int age)            // anything in the () is called a pa
  */
 //----------------------------------------------
 /*
-float cubeaNum(float num);                  //prototyping this function above the main function allows it to be put below the main function
+float cubeaNum(float num);                  // prototyping this function above  main()  allows it to be put below  main()
 
 int main()
 {
@@ -48,10 +48,10 @@ int main()
     printf("%f\n", cubeaNum(2.5555555));
 }
 
-float cubeaNum(float num)                   //this function is called by another function but is below it because...
-{                                           //this function is prototyped above the function that calls it
+float cubeaNum(float num)                   // this function is called by another function but is below it because...
+{                                           // this function is prototyped above the function that calls it
     float answer = num * num * num;
-    return answer;                          //this line could be: return num * num * num;
+    return answer;                          // this line could be:  return num * num * num;
 }
 */
 //----------------------------------------------
@@ -61,8 +61,8 @@ int main()
     int num1, num2, num3;
     printf("Enter numbers:\n");
     scanf("%i\n%i\n%i", &num1, &num2, &num3);
-    if (num1>num2 && num1>num3)             //(num1>num2 || num1>num3)    || is or condition
-    {                                       //(!(num1>num2))    ! around a condition negates that condition, inverts it output
+    if (num1>num2 && num1>num3)             // (num1>num2 || num1>num3)    || is or condition
+    {                                       // (!(num1>num2))    ! around a condition negates that condition, inverts it output
         printf("%i is biggest.", num1);
     }
     else if (num2>num1 && num2>num3)
@@ -102,8 +102,8 @@ int main()
 */
 //----------------------------------------------
 /*
-struct GraphicsCard
-{
+struct GraphicsCard                         // this struct declaration is just an empty framework or scaffolding
+{                                           // multiple structs can be used in your program
     char brand[10];
     char name[15];
     int vram, year;
@@ -111,10 +111,10 @@ struct GraphicsCard
 
 int main()
 {
-    struct GraphicsCard gpu1;
-    strcpy(gpu1.brand, "AMD");
-    strcpy(gpu1.name, "R6900XT");
-    gpu1.vram = 16;
+    struct GraphicsCard gpu1;               // this  gpu1  struct uses the structure of the  GraphicsCard  struct
+    strcpy(gpu1.brand, "AMD");              // the next struct could be  gpu2
+    strcpy(gpu1.name, "R6900XT");           // gpu1  and  gpu2  are to each other what two ints are to each other
+    gpu1.vram = 16;                         // just another instance of the same data type
     gpu1.year = 2020;
     printf("%s %s. Released in %i with %i GB of VRAM.", gpu1.brand, gpu1.name, gpu1.year, gpu1.vram);
     return 0;
@@ -125,23 +125,23 @@ int main()
 int main()
 {
     int i = 1;
-    while (i<10)
+    while (i < 10)
     {
         printf("%i", i);
-        i++;                                //same as i = i + 1;
-    }                                       //i-- is i = i - 1;
+        i++;                                // same as i = i + 1;
+    }                                       // i-- is i = i - 1;
     return 0;
 }
 
 int main()
 {
     int i = 1;
-    do                                      //a do while loop runs the code in the loop before checking the condition
-    {                                       //a while loop checks the condition before executing the code in the loop
+    do                                      // a do while loop runs the code in the loop before checking the condition
+    {                                       // a while loop checks the condition before executing the code in the loop
         printf("%i", i);
         i++;
     }
-    while (i<10);
+    while (i < 10);
     return 0;
 }
 
@@ -149,7 +149,7 @@ int main()
 {
     int i;
     int primeNumbers[] = {2, 3, 5, 7, 11, 13, 17, 19};
-    for (i = 0; i < 8; i++)                 //less than 8 because there are 7 elements in the array
+    for (i = 0; i < 8; i++)                 // less than 8 because there are 7 elements in the array
     {
         printf("%i\n", primeNumbers[i]);
     }
@@ -160,13 +160,13 @@ int main()
 /*
 int main()
 {
-    int nums[3][4] =                        //2D array has 3 rows and 4 columns
+    int nums[3][4] =                        // 2D array has 3 rows and 4 columns
     {
         {2, 4,  6,  8},
         {3, 6,  9, 12},
         {4, 8, 12, 16},
     };
-    //nums[2][1] = 8;                       //this is how you would specify individual elements of 2D array
+    //nums[2][1] = 8;                       // this is how you would specify individual elements of a 2D array
     int i, j;
     for (i = 0; i < 3; i++)
     {
@@ -182,34 +182,34 @@ int main()
 //----------------------------------------------
 /*
 {
-    int myAge = 27;
-    char starType = 'B';
-    printf("I am %i. Our sun is a %c class star.\n", myAge, starType);
-    printf("The physical address in memory of the age variable is %p\n", &myAge);
-    printf("The physical address in memory of the name variable is %p\n", &starType);
-    int * pMyAge = &myAge;                  //a pointer is simply the memory address of a variable
-    char * pStarType = &starType;           //so the variable pmyAge is storing the memory address of the myAge variable
-    printf("%p\n", pMyAge);                 //the variable type for pointer must match the type of the variable that it is the memory address of
-    printf("%i\n", *pMyAge);                //the asterisk dereferences the pointer, so it goes to that memory address and grabs the value stored in there
-
+    int myage = 27;
+    char startype = 'B';
+    printf("I am %i. Our sun is a %c class star.\n", myage, startype);
+    printf("The physical address in memory of the age variable is %p\n", &myage);
+    printf("The physical address in memory of the name variable is %p\n", &startype);
+    int *pmyage = &myage;                   // a pointer is simply the memory address of a variable
+    char *pstartype = &startype;            // so the variable  pmyage  stores the memory address of the  myage  variable
+    printf("%p\n", pmyage);                 // the variable type for pointer must match the type of the variable that it is the memory address of
+    printf("%i\n", *pmyage);                // the asterisk dereferences the pointer, so it goes to that memory address and does some operation
+                                            // it could be to assign a value, get a value, or some other operation
     int i;
-    int *p;    // this is a pointer to an int
-    p = &i;    // use & operator to get a pointer to  i, assign that to  p
-    *p = 3;    // use * operator to "dereference"  p, meaning that 3 is assigned to  i
+    int *p;      // this is a pointer to an int
+    p = &i;      // use & operator to get a pointer to  i, assign that to  p
+    *p = 3;      // use * operator to "dereference"  p, meaning that 3 is assigned to  i
 
-    return 0;    //sometimes you want to know the memory address, so it's convenient to store the address in a variable
+    return 0;    // sometimes you want to know the memory address, so it's convenient to store the address in a variable
 }
 */
 //----------------------------------------------
 /*
 int main()
 {
-    FILE * pFile = fopen("fruits.txt", "w");        //w means write. r means read. a means append.    if the file doesn't exist, write will create it
-                                                    //if no path (absolute or relative) is specified, it will create it where the source code file is
+    FILE * pFile = fopen("fruits.txt", "w");        // w means write. r means read. a means append. if the file doesn't exist, write will create it
+                                                    // if no path (absolute or relative) is specified, it will create it where the source code file is
     fprintf(pFile, "Raspberry, yum\nApple, okay\nDurian, smelly");
-    fopen("fruits.txt", "a");                       //append adds on to the end of a file
+    fopen("fruits.txt", "a");                       // append adds on to the end of a file
     fprintf(pFile, "\nStrawberry, also yum");
-    fclose(pFile);                                  //if you open a file, always close it
+    fclose(pFile);                                  // if you open a file, always close it
     return 0;
 }
 
@@ -217,7 +217,7 @@ int main()
 {
     char line[200];
     FILE * pFile = fopen("fruits.txt", "r");
-    fgets(line, 200, pFile);                //this reads the first 200 characters on the first line of the file and stores them in the line variable
+    fgets(line, 200, pFile);                //this reads the first 200 characters on the first line of the file and stores them in the  line  variable
     printf("%s", line);
     fgets(line, 200, pFile);                //using fgets again will read the second line because it increments after each use
     printf("%s", line);
@@ -229,7 +229,7 @@ int main()
 /*
 int main()
 {
-    int i, j = 4;                           // setting like this is not allowed.  i  will be 0 and j will be 4
+    int i, j = 4;                           // be careful if setting like this,  i  will be garbage and  j  will be 4
     int i = 0, j = 4;                       // setting like this is allowed
 
     printf("var i is %i\n", i);
