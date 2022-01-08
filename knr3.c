31972f518f460e8a7608f8d1bfe78d928482cafe
Blaine Perston <96595637+aSouthGoingZax@users.noreply.github.com>
2022-01-08 17:39:49 +0000

    add file

---
+++ knr3.c
@@ -0,0 +1,15 @@
+/*
+ * The C programming language - K&R
+ * Chapter 3
+ */
+
+#include <stdio.h>
+
+/*
+ * > A statement is any line terminated by a semi-colon.
+ * > Braces group statements together so that they are syntactically equivalent to a single statement.
+ * > if  and  else if  conditions are tests of the numeric value of the expression(s) that forms the condition. If the condition...
+ *   is true, i.e. it evaluates to a non-zero value, the statement for that  if  or  else if  is executed. Testing the numeric...
+ *   value allows for some shortening of conditions.
+ *   - if (expression)  is the same as  if (expression != 0)
+ */
