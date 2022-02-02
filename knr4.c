c40eecfc88520461b6822da1fdbac895d10444fb
Blaine Perston <96595637+aSouthGoingZax@users.noreply.github.com>
2022-02-02 22:42:48 +0000

    start chapter 4

---
+++ knr4.c
@@ -0,0 +1,19 @@
+/*
+ * The C programming language - K&R
+ * Chapter 4
+ */
+
+#include <stdio.h>
+
+/*
+ * > All functions should be declared before they are defined. This allows for type coercions and better error detection.
+ *   - return type, name, parameters:  int strtoint(char input[], short base);
+ *   - The language server will complain when function calls and definition don't match the declaration.
+ * > Function names must be unique.
+ * > Functions that are small and simple are easier to alter and less prone to bugs.
+ *   - They should do one thing, and do it well. This increases their utility to other functions.
+ */
+int main()
+{
+    return 0;
+}
